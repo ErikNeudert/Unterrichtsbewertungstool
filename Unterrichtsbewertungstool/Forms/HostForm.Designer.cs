b8a9438f981f45f3626a02838d837add7de134be
Erik Neudert <eneudert@deloitte.com>
2018-07-03 23:18:00 +0200

    uah

--- Unterrichtsbewertungstool/Forms/HostForm.Designer.cs
+++ Unterrichtsbewertungstool/Forms/HostForm.Designer.cs
@@ -40,9 +40,10 @@
             // 
             // tbxPort
             // 
-            this.tbxPort.Location = new System.Drawing.Point(78, 96);
+            this.tbxPort.Location = new System.Drawing.Point(117, 148);
+            this.tbxPort.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.tbxPort.Name = "tbxPort";
-            this.tbxPort.Size = new System.Drawing.Size(194, 20);
+            this.tbxPort.Size = new System.Drawing.Size(289, 26);
             this.tbxPort.TabIndex = 13;
             this.tbxPort.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             this.tbxPort.TextChanged += new System.EventHandler(this.TbxPort_TextChanged);
@@ -53,18 +54,20 @@
             // 
             this.lblHostTitle.AutoSize = true;
             this.lblHostTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblHostTitle.Location = new System.Drawing.Point(12, 9);
+            this.lblHostTitle.Location = new System.Drawing.Point(18, 14);
+            this.lblHostTitle.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblHostTitle.Name = "lblHostTitle";
-            this.lblHostTitle.Size = new System.Drawing.Size(131, 26);
+            this.lblHostTitle.Size = new System.Drawing.Size(162, 31);
             this.lblHostTitle.TabIndex = 8;
             this.lblHostTitle.Text = "lblHostTitle";
             // 
             // btnhost
             // 
             this.btnhost.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnhost.Location = new System.Drawing.Point(8, 122);
+            this.btnhost.Location = new System.Drawing.Point(12, 188);
+            this.btnhost.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.btnhost.Name = "btnhost";
-            this.btnhost.Size = new System.Drawing.Size(264, 57);
+            this.btnhost.Size = new System.Drawing.Size(396, 88);
             this.btnhost.TabIndex = 14;
             this.btnhost.Text = "btnhost";
             this.btnhost.UseVisualStyleBackColor = true;
@@ -73,10 +76,9 @@
             // cbip
             // 
             this.cbip.FormattingEnabled = true;
-            this.cbip.Location = new System.Drawing.Point(78, 70);
-            this.cbip.Margin = new System.Windows.Forms.Padding(2);
+            this.cbip.Location = new System.Drawing.Point(117, 108);
             this.cbip.Name = "cbip";
-            this.cbip.Size = new System.Drawing.Size(194, 21);
+            this.cbip.Size = new System.Drawing.Size(289, 28);
             this.cbip.TabIndex = 12;
             this.cbip.Text = "cbip";
             this.cbip.SelectedIndexChanged += new System.EventHandler(this.Cbip_SelectedIndexChanged);
@@ -84,9 +86,10 @@
             // 
             // tbxTitel
             // 
-            this.tbxTitel.Location = new System.Drawing.Point(78, 45);
+            this.tbxTitel.Location = new System.Drawing.Point(117, 69);
+            this.tbxTitel.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.tbxTitel.Name = "tbxTitel";
-            this.tbxTitel.Size = new System.Drawing.Size(193, 20);
+            this.tbxTitel.Size = new System.Drawing.Size(288, 26);
             this.tbxTitel.TabIndex = 10;
             this.tbxTitel.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             this.tbxTitel.TextChanged += new System.EventHandler(this.TbxTitel_TextChanged);
@@ -97,9 +100,10 @@
             // 
             this.lblTitle.AutoSize = true;
             this.lblTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblTitle.Location = new System.Drawing.Point(14, 46);
+            this.lblTitle.Location = new System.Drawing.Point(21, 71);
+            this.lblTitle.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblTitle.Name = "lblTitle";
-            this.lblTitle.Size = new System.Drawing.Size(49, 17);
+            this.lblTitle.Size = new System.Drawing.Size(58, 20);
             this.lblTitle.TabIndex = 13;
             this.lblTitle.Text = "lblTitle";
             // 
@@ -107,9 +111,10 @@
             // 
             this.lblIP.AutoSize = true;
             this.lblIP.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblIP.Location = new System.Drawing.Point(14, 71);
+            this.lblIP.Location = new System.Drawing.Point(21, 109);
+            this.lblIP.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblIP.Name = "lblIP";
-            this.lblIP.Size = new System.Drawing.Size(34, 17);
+            this.lblIP.Size = new System.Drawing.Size(41, 20);
             this.lblIP.TabIndex = 14;
             this.lblIP.Text = "lblIP";
             // 
@@ -117,17 +122,18 @@
             // 
             this.lblPort.AutoSize = true;
             this.lblPort.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblPort.Location = new System.Drawing.Point(14, 97);
+            this.lblPort.Location = new System.Drawing.Point(21, 149);
+            this.lblPort.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblPort.Name = "lblPort";
-            this.lblPort.Size = new System.Drawing.Size(48, 17);
+            this.lblPort.Size = new System.Drawing.Size(57, 20);
             this.lblPort.TabIndex = 15;
             this.lblPort.Text = "lblPort";
             // 
             // HostForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 191);
+            this.ClientSize = new System.Drawing.Size(426, 294);
             this.Controls.Add(this.lblPort);
             this.Controls.Add(this.lblIP);
             this.Controls.Add(this.lblTitle);
@@ -136,6 +142,7 @@
             this.Controls.Add(this.tbxPort);
             this.Controls.Add(this.lblHostTitle);
             this.Controls.Add(this.btnhost);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "HostForm";
             this.Text = "Hosten";
             this.ResumeLayout(false);
